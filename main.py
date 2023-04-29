8b10c19a22976f03197c000a1ed43d799688b451
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-04-29 17:19:56 +0800

    can import csv data and reflect on website

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -9,7 +10,32 @@ with col1:
 
 with col2:
     st.title("Elijah Dayon")
-    content = """Hi, I'm Elijah with the ID 119 student from DLSU. Right now I'm developing skill as web developer 
-    to prepare for OJT. Once I settled with OJT, I will plan studying course related to either Artificial Intelligence
+    content = """Hi, I'm Elijah. An ID 119 student from De La Salle University. I'm developing skill 
+    as web developer to prepare for OJT. I will plan studying course related to either Artificial Intelligence
      or GIS for my undergrad thesis"""
     st.info(content)
+
+content2 = """
+The links below can lead to some course works or personal projects I have done in the past.
+"""
+st.write(content2)
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    # extract each row from df
+    for index, row in df[:10].iterrows():
+        st.subheader(row["title"])
+        st.write(row["description"])
+        st.image("images/"+row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+# last 10 rows df[10:]
+# first 10 roes df[:10]
+with col4:
+    for index, row in df[10:].iterrows():
+        st.subheader(row["title"])
+        st.write(row["description"])
+        st.image("images/"+row["image"])
+        st.write(f"[Source Code]({row['url']})")
